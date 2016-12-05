4801f898e90e782b0bbf682ff7775dfa5bf1cc2b
Tobias Ortmayr <tobias.ortmayr@gmail.com>
2016-12-05 17:16:38 +0100

    Reformating

--- src/main/java/at/ac/tuwien/aic/streamprocessing/storm/TridentProcessingTopology.java
+++ src/main/java/at/ac/tuwien/aic/streamprocessing/storm/TridentProcessingTopology.java
@@ -17,43 +17,40 @@ import org.apache.storm.tuple.Fields;
 
 public class TridentProcessingTopology {
 
-	public static StormTopology buildTopology() {
-		Fields taxiFields = new Fields("id", "timestamp", "latitude", "longitude");
-		Fields taxiFieldsWithSpeed = new Fields("id", "timestamp", "latitude", "longitude", "speed");
-		Fields taxiFieldsWithAvgSpeed = new Fields("id", "timestamp", "latitude", "longitude", "speed", "avgSpeed");
-		Fields taxiFieldsWithDistance = new Fields("id", "timestamp", "latitude", "longitude", "distance");
+    public static StormTopology buildTopology() {
+        Fields taxiFields = new Fields("id", "timestamp", "latitude", "longitude");
+        Fields taxiFieldsWithSpeed = new Fields("id", "timestamp", "latitude", "longitude", "speed");
+        Fields taxiFieldsWithAvgSpeed = new Fields("id", "timestamp", "latitude", "longitude", "speed", "avgSpeed");
+        Fields taxiFieldsWithDistance = new Fields("id", "timestamp", "latitude", "longitude", "distance");
 
-		TridentTopology topology = new TridentTopology();
-		TestTaxiFixedDataSpout spout = new TestTaxiFixedDataSpout();
-		Stream inputStream = topology.newStream("taxi", spout);
+        TridentTopology topology = new TridentTopology();
+        TestTaxiFixedDataSpout spout = new TestTaxiFixedDataSpout();
+        Stream inputStream = topology.newStream("taxi", spout);
 
-		inputStream.partitionAggregate(taxiFields, new CalculateSpeed(), taxiFieldsWithSpeed).toStream()
-				.each(taxiFieldsWithSpeed, new Debug("speed"))
-				.partitionAggregate(taxiFieldsWithSpeed, new CalculateAverageSpeed(), taxiFieldsWithAvgSpeed).toStream()
-				.each(taxiFieldsWithAvgSpeed, new Debug("avgSpeed"))
-				.each(taxiFieldsWithAvgSpeed, new StoreInformation(OperatorType.AVERGAGE_SPEED));
+        inputStream.partitionAggregate(taxiFields, new CalculateSpeed(), taxiFieldsWithSpeed).toStream().each(taxiFieldsWithSpeed, new Debug("speed"))
+                .partitionAggregate(taxiFieldsWithSpeed, new CalculateAverageSpeed(), taxiFieldsWithAvgSpeed).toStream()
+                .each(taxiFieldsWithAvgSpeed, new Debug("avgSpeed")).each(taxiFieldsWithAvgSpeed, new StoreInformation(OperatorType.AVERGAGE_SPEED));
 
-		inputStream.partitionAggregate(taxiFields, new CalculateDistance(), taxiFieldsWithDistance).toStream()
-				.each(taxiFieldsWithDistance, new Debug("distance"))
-				.each(taxiFieldsWithDistance, new StoreInformation(OperatorType.DISTANCE));
+        inputStream.partitionAggregate(taxiFields, new CalculateDistance(), taxiFieldsWithDistance).toStream()
+                .each(taxiFieldsWithDistance, new Debug("distance")).each(taxiFieldsWithDistance, new StoreInformation(OperatorType.DISTANCE));
 
-		return topology.build();
-	}
+        return topology.build();
+    }
 
-	public static void main(String[] args) throws Exception {
-		// this method is for testing only
-		Config conf = new Config();
-		conf.setDebug(false);
-		conf.setMaxTaskParallelism(1);
+    public static void main(String[] args) throws Exception {
+        // this method is for testing only
+        Config conf = new Config();
+        conf.setDebug(false);
+        conf.setMaxTaskParallelism(1);
 
-		LocalCluster cluster = new LocalCluster();
-		cluster.submitTopology("stream-processing", conf, buildTopology());
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("stream-processing", conf, buildTopology());
 
-		Thread.sleep(20000);
+        Thread.sleep(20000);
 
-		cluster.shutdown();
+        cluster.shutdown();
 
-		throw new RuntimeException("Exit Zookeper the Hard way");
-	}
+        throw new RuntimeException("Exit Zookeper the Hard way");
+    }
 
 }
--- src/main/java/at/ac/tuwien/aic/streamprocessing/storm/trident/StoreInformation.java
+++ src/main/java/at/ac/tuwien/aic/streamprocessing/storm/trident/StoreInformation.java
@@ -6,37 +6,36 @@ import org.apache.storm.trident.tuple.TridentTuple;
 import redis.clients.jedis.Jedis;
 
 public class StoreInformation extends BaseFilter {
-	private static final String REDIS_HOST = "localhost";
-	private static final int REDIS_PORT = 6379;
+    private static final String REDIS_HOST = "localhost";
+    private static final int REDIS_PORT = 6379;
 
-	public enum OperatorType {
-		AVERGAGE_SPEED, DISTANCE
-	}
+    public enum OperatorType {
+        AVERGAGE_SPEED, DISTANCE
+    }
 
-	private OperatorType type;
+    private OperatorType type;
 
+    public StoreInformation(OperatorType type) {
+        this.type = type;
 
-	public StoreInformation(OperatorType type) {
-		this.type = type;
-		
-	}
+    }
 
-	@Override
-	public boolean isKeep(TridentTuple tuple) {
-		String key="", value="";
-		
-		if (type.equals(OperatorType.AVERGAGE_SPEED)) {
-			key = "" + tuple.getIntegerByField("id") + "_avg";
-			value = "" + tuple.getDoubleByField("avgSpeed");
-		} else if (type.equals(OperatorType.DISTANCE)) {
-			key = "" + tuple.getIntegerByField("id") + "_dist";
-			value = "" + tuple.getDoubleByField("distance");
-		}
-		if (!key.isEmpty() && !value.isEmpty()) {
-			Jedis jedis = new Jedis(REDIS_HOST, REDIS_PORT);
-			jedis.set(key, value);
-			jedis.close();
-		}
-		return false;
-	}
+    @Override
+    public boolean isKeep(TridentTuple tuple) {
+        String key = "", value = "";
+
+        if (type.equals(OperatorType.AVERGAGE_SPEED)) {
+            key = "" + tuple.getIntegerByField("id") + "_avg";
+            value = "" + tuple.getDoubleByField("avgSpeed");
+        } else if (type.equals(OperatorType.DISTANCE)) {
+            key = "" + tuple.getIntegerByField("id") + "_dist";
+            value = "" + tuple.getDoubleByField("distance");
+        }
+        if (!key.isEmpty() && !value.isEmpty()) {
+            Jedis jedis = new Jedis(REDIS_HOST, REDIS_PORT);
+            jedis.set(key, value);
+            jedis.close();
+        }
+        return false;
+    }
 }
