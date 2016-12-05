c733becab3d35fa89ead9f0da0c087b50afdd539
Tobias Ortmayr <tobias.ortmayr@gmail.com>
2016-12-05 16:42:58 +0100

    Prototypical solution for storage in redis.

--- src/main/java/at/ac/tuwien/aic/streamprocessing/storm/TridentProcessingTopology.java
+++ src/main/java/at/ac/tuwien/aic/streamprocessing/storm/TridentProcessingTopology.java
@@ -4,7 +4,9 @@ import at.ac.tuwien.aic.streamprocessing.storm.spout.TestTaxiFixedDataSpout;
 import at.ac.tuwien.aic.streamprocessing.storm.trident.CalculateAverageSpeed;
 import at.ac.tuwien.aic.streamprocessing.storm.trident.CalculateDistance;
 import at.ac.tuwien.aic.streamprocessing.storm.trident.CalculateSpeed;
-import at.ac.tuwien.aic.streamprocessing.storm.trident.RedisFunction;
+import at.ac.tuwien.aic.streamprocessing.storm.trident.StoreInformation;
+import at.ac.tuwien.aic.streamprocessing.storm.trident.StoreInformation.OperatorType;
+
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
 import org.apache.storm.generated.StormTopology;
@@ -15,48 +17,43 @@ import org.apache.storm.tuple.Fields;
 
 public class TridentProcessingTopology {
 
-    public static StormTopology buildTopology() {
-        Fields taxiFields = new Fields("id", "timestamp", "latitude", "longitude");
-        Fields taxiFieldsWithSpeed = new Fields("id", "timestamp", "latitude", "longitude","speed");
-        Fields taxiFieldsWithAvgSpeed = new Fields("id", "timestamp", "latitude", "longitude","speed","avgSpeed");
-        Fields taxiFieldsWithDistance = new Fields("id", "timestamp", "latitude", "longitude","distance");
+	public static StormTopology buildTopology() {
+		Fields taxiFields = new Fields("id", "timestamp", "latitude", "longitude");
+		Fields taxiFieldsWithSpeed = new Fields("id", "timestamp", "latitude", "longitude", "speed");
+		Fields taxiFieldsWithAvgSpeed = new Fields("id", "timestamp", "latitude", "longitude", "speed", "avgSpeed");
+		Fields taxiFieldsWithDistance = new Fields("id", "timestamp", "latitude", "longitude", "distance");
 
-        TridentTopology topology = new TridentTopology();
-        TestTaxiFixedDataSpout spout = new TestTaxiFixedDataSpout();
-        Stream inputStream = topology.newStream("taxi", spout);
+		TridentTopology topology = new TridentTopology();
+		TestTaxiFixedDataSpout spout = new TestTaxiFixedDataSpout();
+		Stream inputStream = topology.newStream("taxi", spout);
 
-        inputStream
-                .partitionAggregate(taxiFields, new CalculateSpeed(), taxiFieldsWithSpeed)
-                .toStream()
-                .each(taxiFieldsWithSpeed, new Debug("speed"))
-                .partitionAggregate(taxiFieldsWithSpeed, new CalculateAverageSpeed(), taxiFieldsWithAvgSpeed)
-                .toStream()
-                .each(taxiFieldsWithSpeed, new Debug("avgSpeed"))
-                .each(taxiFieldsWithSpeed, new RedisFunction());
+		inputStream.partitionAggregate(taxiFields, new CalculateSpeed(), taxiFieldsWithSpeed).toStream()
+				.each(taxiFieldsWithSpeed, new Debug("speed"))
+				.partitionAggregate(taxiFieldsWithSpeed, new CalculateAverageSpeed(), taxiFieldsWithAvgSpeed).toStream()
+				.each(taxiFieldsWithAvgSpeed, new Debug("avgSpeed"))
+				.each(taxiFieldsWithAvgSpeed, new StoreInformation(OperatorType.AVERGAGE_SPEED));
 
-        inputStream
-                .partitionAggregate(taxiFields, new CalculateDistance(), taxiFieldsWithDistance)
-                .toStream()
-                .each(taxiFieldsWithDistance, new Debug("distance"))
-                .each(taxiFieldsWithDistance, new RedisFunction());
+		inputStream.partitionAggregate(taxiFields, new CalculateDistance(), taxiFieldsWithDistance).toStream()
+				.each(taxiFieldsWithDistance, new Debug("distance"))
+				.each(taxiFieldsWithDistance, new StoreInformation(OperatorType.DISTANCE));
 
-        return topology.build();
-    }
+		return topology.build();
+	}
 
-    public static void main(String[] args) throws Exception {
-        // this method is for testing only
-        Config conf = new Config();
-        conf.setDebug(false);
-        conf.setMaxTaskParallelism(1);
+	public static void main(String[] args) throws Exception {
+		// this method is for testing only
+		Config conf = new Config();
+		conf.setDebug(false);
+		conf.setMaxTaskParallelism(1);
 
-        LocalCluster cluster = new LocalCluster();
-        cluster.submitTopology("stream-processing", conf, buildTopology());
+		LocalCluster cluster = new LocalCluster();
+		cluster.submitTopology("stream-processing", conf, buildTopology());
 
-        Thread.sleep(20000);
+		Thread.sleep(20000);
 
-        cluster.shutdown();
+		cluster.shutdown();
 
-        throw new RuntimeException("Exit Zookeper the Hard way");
-    }
+		throw new RuntimeException("Exit Zookeper the Hard way");
+	}
 
 }
