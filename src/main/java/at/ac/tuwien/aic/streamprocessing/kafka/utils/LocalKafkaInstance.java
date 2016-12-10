989a98bc5cf6c2e81a5ad726662779670359e6bc
Kristoffer Kleine <kristoffer.kleine@tuwien.ac.at>
2016-12-10 17:15:33 +0100

    Create temporary log directory for use in tests and local testing

--- src/main/java/at/ac/tuwien/aic/streamprocessing/kafka/utils/LocalKafkaInstance.java
+++ src/main/java/at/ac/tuwien/aic/streamprocessing/kafka/utils/LocalKafkaInstance.java
@@ -7,6 +7,7 @@ import kafka.utils.ZKStringSerializer$;
 import kafka.utils.ZkUtils;
 import org.I0Itec.zkclient.ZkClient;
 import org.I0Itec.zkclient.ZkConnection;
+import org.apache.commons.io.FileUtils;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.BoundedExponentialBackoffRetry;
@@ -16,14 +17,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Properties;
 
 
 public class LocalKafkaInstance {
     private final static Logger logger = LoggerFactory.getLogger(LocalKafkaInstance.class);
+    private final static String TMP_LOG_PREFIX = "aic-stream";
 
     private final int kafka_port;
     private final int zookeeper_port;
+
+    private Path temporaryLogDir;
 
     private TestingServer zookeeperServer;
     private KafkaServerStartable kafkaServer;
@@ -31,6 +38,13 @@ public class LocalKafkaInstance {
     public LocalKafkaInstance(int kafka_port, int zookeeper_port) {
         this.kafka_port = kafka_port;
         this.zookeeper_port = zookeeper_port;
+
+        try {
+            temporaryLogDir = Files.createTempDirectory(TMP_LOG_PREFIX);
+        } catch (IOException e) {
+            logger.error("Failed to create temporary zookeeper and kafka log directory. Aborting", e);
+            throw new RuntimeException(e);
+        }
     }
 
     public static LocalKafkaInstance createDefault() {
@@ -47,7 +61,8 @@ public class LocalKafkaInstance {
 
     public void start() throws Exception {
         System.out.println("Starting zookeeper.");
-        this.zookeeperServer = new TestingServer(zookeeper_port, new File("/tmp/zookeeper-logs"));
+        File zookeeperLogDir = temporaryLogDir.resolve("zookeeper-logs").toFile();
+        this.zookeeperServer = new TestingServer(zookeeper_port, zookeeperLogDir);
         System.out.println("Started zookeeper.");
 
         ExponentialBackoffRetry retryPolicy = new BoundedExponentialBackoffRetry(200, 5000, 5);
@@ -55,12 +70,14 @@ public class LocalKafkaInstance {
                 zookeeperServer.getConnectString(), retryPolicy);
         zookeeper.start();
 
+        String kafkaLogDir = temporaryLogDir.resolve("kafka-logs").toString();
+
         Properties p = new Properties();
         p.setProperty("zookeeper.connect", zookeeperServer.getConnectString());
         p.setProperty("broker.id", "0");
         p.setProperty("port", "" + kafka_port);
         p.setProperty("delete.topic.enable", "true");
-        p.setProperty("log.dir", "/tmp/kafka-logs");
+        p.setProperty("log.dir", kafkaLogDir);
         p.setProperty("log.retention.ms", "30000"); // 30 seconds retention
         KafkaConfig config = new KafkaConfig(p);
 
@@ -80,6 +97,8 @@ public class LocalKafkaInstance {
         this.zookeeperServer.close();
         this.zookeeperServer.stop();
         System.out.println("Shut down zookeeper");
+
+        FileUtils.deleteDirectory(temporaryLogDir.toFile());
     }
 
     public void createTopic(String topicName) {
