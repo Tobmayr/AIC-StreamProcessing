2df62acd80ff87310fe8b96561a56ecbdaae83bc
Enri Miho <e0929003@student.tuwien.ac.at>
2016-12-10 03:38:13 +0100

    implement Serializer so that we can use this class directly in config files. also encode in Base64.

--- src/main/java/at/ac/tuwien/aic/streamprocessing/model/serialization/TaxiEntrySerializer.java
+++ src/main/java/at/ac/tuwien/aic/streamprocessing/model/serialization/TaxiEntrySerializer.java
@@ -1,34 +1,53 @@
 package at.ac.tuwien.aic.streamprocessing.model.serialization;
 
 import at.ac.tuwien.aic.streamprocessing.model.TaxiEntry;
+import org.apache.kafka.common.serialization.Serializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.Base64;
+import java.util.Map;
 
 /**
  * Serializer for taxi entries.
  */
-public class TaxiEntrySerializer {
+public class TaxiEntrySerializer implements Serializer<TaxiEntry> {
     private final static Logger logger = LoggerFactory.getLogger(TaxiEntrySerializer.class);
+
+    private boolean isKey;
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+        this.isKey = isKey;
+    }
 
     /**
      * Serialize taxi entry to byte[].
      *
-     * @param entry the entry which to serialize
+     * @param data the entry which to serialize
      * @return the resulting byte[]
      */
-    public static byte[] serialize(TaxiEntry entry) {
-        try (ByteArrayOutputStream byteArrayStream = new ByteArrayOutputStream()) {
-            try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayStream)) {
-                objectOutputStream.writeObject(entry);
-            }
-            return byteArrayStream.toByteArray();
-        } catch (IOException e) {
-            logger.debug("Failed to serialize " + entry.toString(), e);
+    @Override
+    public byte[] serialize(String topic, TaxiEntry data) {
+        if (data == null) {
             return null;
         }
+        try (ByteArrayOutputStream byteArrayStream = new ByteArrayOutputStream()) {
+            try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayStream)) {
+                objectOutputStream.writeObject(data);
+            }
+            return Base64.getEncoder().encode(byteArrayStream.toByteArray());
+        } catch (IOException e) {
+            logger.debug("Failed to serialize " + data.toString(), e);
+            return null;
+        }
+    }
+
+    @Override
+    public void close() {
+
     }
 }
