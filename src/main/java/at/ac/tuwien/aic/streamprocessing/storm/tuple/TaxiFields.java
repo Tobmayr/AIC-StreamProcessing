7d8cc1d0411da5dae8b64c0eb054d1ffc199d43c
Kristoffer Kleine <kristoffer.kleine@tuwien.ac.at>
2016-12-10 18:42:28 +0100

    Fix automatic refactoring error

--- src/main/java/at/ac/tuwien/aic/streamprocessing/storm/tuple/TaxiFields.java
+++ src/main/java/at/ac/tuwien/aic/streamprocessing/storm/tuple/TaxiFields.java
@@ -6,5 +6,5 @@ public class TaxiFields {
     public static Fields BASE_FIELDS = new Fields("id", "timestamp", "latitude", "longitude");
     public static Fields BASE_SPEED_FIELDS = new Fields("id", "timestamp", "latitude", "longitude", "speed");
     public static Fields BASE_SPEED_AVG_FIELDS = new Fields("id", "timestamp", "latitude", "longitude", "speed", "avgSpeed");
-    public static Fields BASE_DISTANCE_FIELDS = new Fields("id", "timestamp", "latitude", "longitude", "BASE_DISTANCE_FIELDS");
+    public static Fields BASE_DISTANCE_FIELDS = new Fields("id", "timestamp", "latitude", "longitude", "distance");
 }
