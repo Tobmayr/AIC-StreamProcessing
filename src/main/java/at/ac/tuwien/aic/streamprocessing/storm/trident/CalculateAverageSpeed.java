95be04217f68b56060f0044d9390d0bdcf9c2c83
Arber Kryeziu <e0825135@student.tuwien.ac.at>
2016-12-14 00:53:39 +0100

    added logging for Bolt - CalculateAverageSpeed

--- src/main/java/at/ac/tuwien/aic/streamprocessing/storm/trident/CalculateAverageSpeed.java
+++ src/main/java/at/ac/tuwien/aic/streamprocessing/storm/trident/CalculateAverageSpeed.java
@@ -1,20 +1,24 @@
 package at.ac.tuwien.aic.streamprocessing.storm.trident;
 
 import at.ac.tuwien.aic.streamprocessing.model.utils.Timestamp;
+import at.ac.tuwien.aic.streamprocessing.storm.TridentProcessingTopology;
 import org.apache.storm.trident.operation.TridentCollector;
 import org.apache.storm.trident.tuple.TridentTuple;
 import org.apache.storm.tuple.Values;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
 
 public class CalculateAverageSpeed extends LastState<CalculateAverageSpeed.TaxiAvgSpeed> {
-
     /**
      * The _Calculate speed_ operator calculates the speed between two successive locations for each taxi, whereas the
      * distance between two locations can be derived by the Haversine formula{4}. This operator represents a stateful
      * operator because it is required to always remember the last location of the taxi to calculate the current
      * speed[1]
      */
+
+    private final Logger logger = LoggerFactory.getLogger(CalculateAverageSpeed.class);
 
     class TaxiAvgSpeed {
         String lastTimestamp;
@@ -41,7 +45,7 @@ public class CalculateAverageSpeed extends LastState<CalculateAverageSpeed.TaxiA
             Double time;
 
             if (oldTime.isAfter(newTime) || oldTime.isEqual(newTime)) {
-                System.out.println("Old tuple is not older than new one!");
+                logger.debug("Old tuple is not older than new one!");
 
                 // since it is not meaningful to compute the time in this case, just use a default value of 0.0
                 time = 0.0;
@@ -58,6 +62,7 @@ public class CalculateAverageSpeed extends LastState<CalculateAverageSpeed.TaxiA
             }
 
             collector.emit(new Values(id, newAvgSpeed.lastTimestamp, latitude, longitude, speed, newAvgSpeed.avgSpeed));
+            logger.debug("(avgSpeed): [" + id + ", " + newAvgSpeed.lastTimestamp + ", " + latitude + ", " + longitude + ", " + speed + ", " + newAvgSpeed.avgSpeed + "]");
         }
 
         return newAvgSpeed;
