aa48e6e2179d1e606af1949df8950c333a329084
Andreas Kern <andreas.kern@gmail.com>
2017-01-10 19:29:07 +0100

    Show Server port after starting it

--- src/main/resources/dashboard/server.js
+++ src/main/resources/dashboard/server.js
@@ -42,4 +42,6 @@ app.post('/incident', function (req, res) {
    res.send('success');
 });
 
-server.listen(3000);
+server.listen(3000, function () {
+   console.log('Server started on port 3000')
+});
