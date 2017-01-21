888c5c0f0acbb54e041b7d35d5be480d1d2b89c6
Enri Miho <e0929003@student.tuwien.ac.at>
2017-01-21 14:37:24 +0100

    undo changes.

--- src/main/java/at/ac/tuwien/aic/streamprocessing/storm/trident/dashboard/AreaLeavingNotifier.java
+++ src/main/java/at/ac/tuwien/aic/streamprocessing/storm/trident/dashboard/AreaLeavingNotifier.java
@@ -1,22 +1,16 @@
 package at.ac.tuwien.aic.streamprocessing.storm.trident.dashboard;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import org.apache.storm.trident.tuple.TridentTuple;
-
 import at.ac.tuwien.aic.streamprocessing.storm.trident.util.Constants;
 import at.ac.tuwien.aic.streamprocessing.storm.trident.util.Haversine;
 
 public class AreaLeavingNotifier extends DashboardNotifier {
 
-    private String dashboardAddress;
-
     public AreaLeavingNotifier(String dashboardAddress) {
         super(dashboardAddress + Constants.NOTIFY_AREA_VIOLATION_URI);
-        this.dashboardAddress = dashboardAddress;
     }
-
+    
     @Override
     public boolean isKeep(TridentTuple tuple) {
         Integer taxiId = tuple.getIntegerByField("id");
@@ -25,19 +19,12 @@ public class AreaLeavingNotifier extends DashboardNotifier {
         Double latitude = tuple.getDoubleByField("latitude");
         Double longitude = tuple.getDoubleByField("longitude");
         Double distance = Haversine.calculateDistanceBetween(centerLat, centerLong, latitude, longitude);
-
-        Map<String, String> map = new HashMap<>();
-        map.put("taxiId", Integer.toString(taxiId));
-        map.put("distance", Double.toString(distance));
-
-        if (distance >= Constants.WARNING_DISTANCE && distance < Constants.BORDER_DISTANCE) {
+        if (distance >= Constants.WARNING_DISTANCE) {
+            Map<String, String> map = new HashMap<>();
+            map.put("taxiId", Integer.toString(taxiId));
+            map.put("distance", Double.toString(distance));
             sendJSONPostRequest(map);
-        } else if (distance > Constants.BORDER_DISTANCE){
-            sendJSONPostRequest(map, dashboardAddress + Constants.NOTIFY_TAXI_STOPPED_URI);
-            return false;
         }
         return true;
-
     }
-
 }
