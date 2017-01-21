7349e18b403908e3a6daca0cbdea997a62754ba4
Arber Kryeziu <e0825135@student.tuwien.ac.at>
2017-01-21 14:25:33 +0100

    notify dashboard when taxi rides beyond 15km border

--- src/main/java/at/ac/tuwien/aic/streamprocessing/storm/trident/dashboard/AreaLeavingNotifier.java
+++ src/main/java/at/ac/tuwien/aic/streamprocessing/storm/trident/dashboard/AreaLeavingNotifier.java
@@ -10,8 +10,11 @@ import at.ac.tuwien.aic.streamprocessing.storm.trident.util.Haversine;
 
 public class AreaLeavingNotifier extends DashboardNotifier {
 
+    private String dashboardAddress;
+
     public AreaLeavingNotifier(String dashboardAddress) {
         super(dashboardAddress + Constants.NOTIFY_AREA_VIOLATION_URI);
+        this.dashboardAddress = dashboardAddress;
     }
 
     @Override
@@ -22,12 +25,15 @@ public class AreaLeavingNotifier extends DashboardNotifier {
         Double latitude = tuple.getDoubleByField("latitude");
         Double longitude = tuple.getDoubleByField("longitude");
         Double distance = Haversine.calculateDistanceBetween(centerLat, centerLong, latitude, longitude);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("taxiId", Integer.toString(taxiId));
+        map.put("distance", Double.toString(distance));
+
         if (distance >= Constants.WARNING_DISTANCE && distance < Constants.BORDER_DISTANCE) {
-            Map<String, String> map = new HashMap<>();
-            map.put("taxiId", Integer.toString(taxiId));
-            map.put("distance", Double.toString(distance));
             sendJSONPostRequest(map);
         } else if (distance > Constants.BORDER_DISTANCE){
+            sendJSONPostRequest(map, dashboardAddress + Constants.NOTIFY_TAXI_STOPPED_URI);
             return false;
         }
         return true;
