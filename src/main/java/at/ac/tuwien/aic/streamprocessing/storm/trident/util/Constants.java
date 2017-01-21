165ce8d87f5183188320afcb7fe86faec013d886
Arber Kryeziu <e0825135@student.tuwien.ac.at>
2017-01-21 01:35:06 +0100

    added border constant  15km

--- src/main/java/at/ac/tuwien/aic/streamprocessing/storm/trident/util/Constants.java
+++ src/main/java/at/ac/tuwien/aic/streamprocessing/storm/trident/util/Constants.java
@@ -9,6 +9,7 @@ public class Constants {
     public static final Double ALLOWED_AREA_CENTER_LAT = 39.916320;
     public static final Double ALLOWED_AREA_CENTER_LONG = 116.397155;
     public static final Double WARNING_DISTANCE = 10D;
+    public static final Double BORDER_DISTANCE = 15D;
 
     // Speeding configuration
     public static final Double SPEED_LIMIT = 50D;
